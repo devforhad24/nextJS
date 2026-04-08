a20167aa778e4dab84b76fe6bbdeafde5f2d581a
Forhad <developerforhad24@gmail.com>
2026-04-08 08:11:30 +0600

    UI - User interface in jsx (reactJS)

---
+++ react-tutorial/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App(){
+  let name = "Forhad Hossain"
+  return(
+    <div className='header'>
+      <h1>React Tutorial: {name}</h1>
+    </div>
+  )
+}
+
+export default App;
