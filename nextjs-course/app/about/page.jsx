e2b112ca039a349ab03cce801ee1e5d10cb498c0
Forhad <developerforhad24@gmail.com>
2026-04-08 23:02:09 +0600

    App Router - create routes

---
+++ nextjs-course/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+  return(
+    <div>
+      <h1>About Page</h1>
+      <p>About page - dolor sit amet consectetur adipisicing elit. Odio eveniet debitis repudian.</p>
+    </div>
+  )
+}
