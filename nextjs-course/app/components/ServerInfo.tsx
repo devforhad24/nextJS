3c686cbb3b5aadc4e8047f411ed97d724614dd43
Forhad <developerforhad24@gmail.com>
2026-04-09 19:29:31 +0600

    Static vs Dynamic server components

--- nextjs-course/app/components/ServerInfo.tsx
+++
@@ -1,9 +0,0 @@
-export default function ServerInfo(){
-    const serverTime = new Date().toLocaleTimeString();
-
-    return(
-        <div>
-            <h1>Server Time: {serverTime}</h1>
-        </div>
-    )
-}
